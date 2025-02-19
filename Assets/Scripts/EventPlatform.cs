ac1bb59d6122236fcc557ab7579a481e62b94501
Pgt103 <ddh06095@gmail.com>
2025-02-19 21:17:10 +0900

    [feat] 발판에 접촉하면 말풍선 등장 및 다른 씬 이동 기능 추가
    발판에 접촉하면 말풍선이 등장하고 그 상태에서 E를 누르면 다른 씬으로 이동하는 기능을 추가. 원래 목적과는 조금 다른 구현이라 수리 예정

---
+++ Assets/Scripts/EventPlatform.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // 씬 전환을 위해 필요
+
+public class EventPlatform : MonoBehaviour
+{
+    public GameObject speechBubble; // 말풍선 UI
+
+    private bool isPlayerOnPlatform = false;
+
+    void Start()
+    {
+        if (speechBubble != null)
+        {
+            speechBubble.SetActive(false); // 초기에는 말풍선 비활성화
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) // 플레이어가 닿으면
+        {
+            isPlayerOnPlatform = true;
+            if (speechBubble != null)
+            {
+                Debug.Log("말풍선 활성화");
+                speechBubble.SetActive(true); // 말풍선 UI 활성화
+            }
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) // 플레이어가 떠나면
+        {
+            isPlayerOnPlatform = false;
+            if (speechBubble != null)
+            {
+                Debug.Log("말풍선 비활성화");
+                speechBubble.SetActive(false); // 말풍선 UI 비활성화
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (isPlayerOnPlatform && Input.GetKeyDown(KeyCode.E)) // E키를 누르면
+        {
+            SceneManager.LoadScene("Minigame1"); // 원하는 씬으로 변경
+        }
+    }
+}
